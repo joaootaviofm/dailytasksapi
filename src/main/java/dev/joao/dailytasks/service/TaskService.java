c5c86c36973d0b20232cd631d78ec11254df53f8
joaootaviofm <joaootaviofm@gmail.com>
2025-03-31 18:53:42 -0300

    added service methods

--- src/main/java/dev/joao/dailytasks/service/TaskService.java
+++ src/main/java/dev/joao/dailytasks/service/TaskService.java
@@ -1,13 +1,49 @@
 package dev.joao.dailytasks.service;
 
+import dev.joao.dailytasks.model.Task;
 import dev.joao.dailytasks.repository.TaskRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class TaskService {
 
-    TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+
+    public Task createNewTask(Task task){
+        if(task == null) {
+            throw new IllegalArgumentException("Task cannot be null");
+        }
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTask(UUID id){
+        return taskRepository.findById(id);
+    }
+
+    public void deleteTask(UUID id){
+        taskRepository.deleteById(id);
+    }
+
+    public void deleteTasks(){
+        taskRepository.deleteAll();
+    }
+
+
+
+
+
+
 
 
 }
