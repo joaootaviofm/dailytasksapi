f936d2144444d7df0723333d2337eb71aa0b5840
joaootaviofm <joaootaviofm@gmail.com>
2025-03-31 18:54:36 -0300

    refactor: indenting properly

--- src/main/java/dev/joao/dailytasks/repository/TaskRepository.java
+++ src/main/java/dev/joao/dailytasks/repository/TaskRepository.java
@@ -2,8 +2,10 @@ package dev.joao.dailytasks.repository;
 
 import dev.joao.dailytasks.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
